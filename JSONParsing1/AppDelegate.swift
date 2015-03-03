af67becb392597513ee20e71b7b59bcec8cdf7c0
Girish <girish.potnuru@paradigmcreatives.com>
2015-03-03 23:51:59 +0530

    ssseeeeesss

--- JSONParsing1/AppDelegate.swift
+++ JSONParsing1/AppDelegate.swift
@@ -32,7 +32,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func applicationWillEnterForeground(application: UIApplication) {
         // Called as part of the transition from the background to the inactive state; here you can undo many of the changes made on entering the background.
         
-        //This is list kkkkkkk
+        //This is list kkkkkkk ///
     }
 
     func applicationDidBecomeActive(application: UIApplication) {
